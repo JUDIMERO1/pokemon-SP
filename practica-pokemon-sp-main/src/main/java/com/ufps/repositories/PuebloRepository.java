1ce7c57e9d2de56da58baaf5d26b56581eb5615e
JUAN DIEGO M <131618404+JUDIMERO1@users.noreply.github.com>
2024-07-10 08:52:59 -0500

    Delete practica-pokemon-sp-main/src directory

--- practica-pokemon-sp-main/src/main/java/com/ufps/repositories/PuebloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ufps.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ufps.entities.Pueblo;
-
-public interface PuebloRepository extends JpaRepository<Pueblo, Integer> {
-
-}
--- practica-pokemon-sp-main/src/main/java/com/ufps/repositories/TipoPokemonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ufps.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ufps.entities.TipoPokemon;
-
-public interface TipoPokemonRepository extends JpaRepository<TipoPokemon, Integer> {
-
-}
